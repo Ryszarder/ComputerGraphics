692b768a545390355653016b39ad9759a36e9633
Blake P <s210561@students.aie.edu.au>
2022-04-04 11:36:11 +1000

    change lighting normal so colours alpha sets to zero

--- Main/Graphics Engine/Graphics Engine/ImageRender.cpp
+++ Main/Graphics Engine/Graphics Engine/ImageRender.cpp
@@ -12,55 +12,57 @@ ImageRender::ImageRender()
 	glGenBuffers(1, &triangleID);
 
 	//Cube
-	Vertex vertices[36];
-	vertices[0].m_Vposition  = {0.0f, 0.0f, 1.0f};	vertices[0].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[0].m_Vnormal  = {0.0f, 0.0f, 1.0f};
-	vertices[1].m_Vposition  = {0.0f, 1.0f, 1.0f};	vertices[1].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[1].m_Vnormal  = {0.0f, 0.0f, 1.0f};
-	vertices[2].m_Vposition  = {1.0f, 0.0f, 1.0f};	vertices[2].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[2].m_Vnormal  = {0.0f, 0.0f, 1.0f};
-	
-	vertices[3].m_Vposition  = {0.0f, 1.0f, 1.0f};	vertices[3].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[3].m_Vnormal  = {0.0f, 0.0f, 1.0f};
-	vertices[4].m_Vposition  = {1.0f, 1.0f, 1.0f};	vertices[4].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[4].m_Vnormal  = {0.0f, 0.0f, 1.0f};
-	vertices[5].m_Vposition  = {1.0f, 0.0f, 1.0f};	vertices[5].m_Vcolour  = {1.0f, 0.0f, 0.0f};	vertices[5].m_Vnormal  = {0.0f, 0.0f, 1.0f};
+	Vertex vertices[] = {
 
-	vertices[6].m_Vposition  = {0.0f, 0.0f, 0.0f};	vertices[6].m_Vcolour  = {0.0f, 0.0f, 1.0f};	vertices[6].m_Vnormal  = {0.0f, 0.0f, -1.0f};
-	vertices[7].m_Vposition  = {0.0f, 1.0f, 0.0f};	vertices[7].m_Vcolour  = {0.0f, 0.0f, 1.0f};	vertices[7].m_Vnormal  = {0.0f, 0.0f, -1.0f};
-	vertices[8].m_Vposition  = {1.0f, 0.0f, 0.0f};	vertices[8].m_Vcolour  = {0.0f, 0.0f, 1.0f};	vertices[8].m_Vnormal  = {0.0f, 0.0f, -1.0f};
+		{{0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+		{{0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+		{{1.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
 
-	vertices[9].m_Vposition  = {0.0f, 1.0f, 0.0f};	vertices[9].m_Vcolour  = {0.0f, 0.0f, 1.0f};    vertices[9].m_Vnormal  = {0.0f, 0.0f, -1.0f};
-	vertices[10].m_Vposition = {1.0f, 1.0f, 0.0f};	vertices[10].m_Vcolour = {0.0f, 0.0f, 1.0f};	vertices[10].m_Vnormal = {0.0f, 0.0f, -1.0f};
-	vertices[11].m_Vposition = {1.0f, 0.0f, 0.0f};	vertices[11].m_Vcolour = {0.0f, 0.0f, 1.0f};	vertices[11].m_Vnormal = {0.0f, 0.0f, -1.0f};
+		{{0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+		{{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+		{{1.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
 
-	vertices[12].m_Vposition = {0.0f, 0.0f, 0.0f};	vertices[12].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[12].m_Vnormal = {-1.0f, 0.0f, 0.0f};
-	vertices[13].m_Vposition = {0.0f, 0.0f, 1.0f};	vertices[13].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[13].m_Vnormal = {-1.0f, 0.0f, 0.0f};
-	vertices[14].m_Vposition = {0.0f, 1.0f, 0.0f};	vertices[14].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[14].m_Vnormal = {-1.0f, 0.0f, 0.0f};
+		{{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+		{{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+		{{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
 
-	vertices[15].m_Vposition = {0.0f, 0.0f, 1.0f};	vertices[15].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[15].m_Vnormal = {-1.0f, 0.0f, 0.0f};
-	vertices[16].m_Vposition = {0.0f, 1.0f, 1.0f};	vertices[16].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[16].m_Vnormal = {-1.0f, 0.0f, 0.0f};
-	vertices[17].m_Vposition = {0.0f, 1.0f, 0.0f};	vertices[17].m_Vcolour = {0.0f, 1.0f, 0.0f};	vertices[17].m_Vnormal = {-1.0f, 0.0f, 0.0f};
+		{{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+		{{1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
+		{{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}},
 
-	vertices[18].m_Vposition = {1.0f, 0.0f, 0.0f};	vertices[18].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[18].m_Vnormal = {1.0f, 0.0f, 0.0f};
-	vertices[19].m_Vposition = {1.0f, 0.0f, 1.0f};	vertices[19].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[19].m_Vnormal = {1.0f, 0.0f, 0.0f};
-	vertices[20].m_Vposition = {1.0f, 1.0f, 0.0f};	vertices[20].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[20].m_Vnormal = {1.0f, 0.0f, 0.0f};
+		{{0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+		{{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+		{{0.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
 
-	vertices[21].m_Vposition = {1.0f, 0.0f, 1.0f};	vertices[21].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[21].m_Vnormal = {1.0f, 0.0f, 0.0f};
-	vertices[22].m_Vposition = {1.0f, 1.0f, 1.0f};	vertices[22].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[22].m_Vnormal = {1.0f, 0.0f, 0.0f};
-	vertices[23].m_Vposition = {1.0f, 1.0f, 0.0f};	vertices[23].m_Vcolour = {0.0f, 1.0f, 1.0f};	vertices[23].m_Vnormal = {1.0f, 0.0f, 0.0f};
+		{{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+		{{0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
+		{{0.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}},
 
-	vertices[24].m_Vposition = {0.0f, 0.0f, 0.0f};	vertices[24].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[24].m_Vnormal = {0.0f, -1.0f, 0.0f};
-	vertices[25].m_Vposition = {0.0f, 0.0f, 1.0f};	vertices[25].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[25].m_Vnormal = {0.0f, -1.0f, 0.0f};
-	vertices[26].m_Vposition = {1.0f, 0.0f, 0.0f};	vertices[26].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[26].m_Vnormal = {0.0f, -1.0f, 0.0f};
+		{{1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+		{{1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+		{{1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
 
-	vertices[27].m_Vposition = {0.0f, 0.0f, 1.0f};	vertices[27].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[27].m_Vnormal = {0.0f, -1.0f, 0.0f};
-	vertices[28].m_Vposition = {1.0f, 0.0f, 1.0f};	vertices[28].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[28].m_Vnormal = {0.0f, -1.0f, 0.0f};
-	vertices[29].m_Vposition = {1.0f, 0.0f, 0.0f};	vertices[29].m_Vcolour = {1.0f, 1.0f, 0.0f};	vertices[29].m_Vnormal = {0.0f, -1.0f, 0.0f};
+		{{1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+		{{1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+		{{1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
 
-	vertices[30].m_Vposition = {0.0f, 1.0f, 0.0f};	vertices[30].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[30].m_Vnormal = {0.0f, 1.0f, 0.0f};
-	vertices[31].m_Vposition = {0.0f, 1.0f, 1.0f};	vertices[31].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[31].m_Vnormal = {0.0f, 1.0f, 0.0f};
-	vertices[32].m_Vposition = {1.0f, 1.0f, 0.0f};	vertices[32].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[32].m_Vnormal = {0.0f, 1.0f, 0.0f};
+		{{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
+		{{0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
+		{{1.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
 
-	vertices[33].m_Vposition = {0.0f, 1.0f, 1.0f};	vertices[33].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[33].m_Vnormal = {0.0f, 1.0f, 0.0f};
-	vertices[34].m_Vposition = {1.0f, 1.0f, 1.0f};	vertices[34].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[34].m_Vnormal = {0.0f, 1.0f, 0.0f};
-	vertices[35].m_Vposition = {1.0f, 1.0f, 0.0f};	vertices[35].m_Vcolour = {1.0f, 0.0f, 1.0f};	vertices[35].m_Vnormal = {0.0f, 1.0f, 0.0f};
+		{{0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f},{0.0f, -1.0f, 0.0f}},
+		{{1.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
+		{{1.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}},
 
+		{{0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+		{{0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+		{{1.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+
+		{{0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+		{{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+		{{1.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+
+	};
 	glBindBuffer(GL_ARRAY_BUFFER, triangleID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -96,13 +98,13 @@ void ImageRender::Draw(ShaderProgram meshShader, ShaderProgram lightShader)
 	glm::mat4 projection = glm::perspective(3.14159f / 4, 1920.0f / 1080, 0.1f, 100.0f);
 	glm::mat4 view = glm::lookAt(glm::vec3(5, 5, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	glm::mat4 rotImage = glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0, 1, 0));
-	glm::vec3 lightPos(1.0f, 2.0f, 0.0f);
+	glm::vec3 lightPos(2.0f, 2.0f, 0.0f);
 
 	meshShader.SetUniform("mvpMatrix", projection * view * rotImage);
 	meshShader.SetUniform("modelMatrix", rotImage);
 	meshShader.SetUniform("lightPos", lightPos);
 	meshShader.SetUniform("viewPos", glm::vec3(5, 5, 5));
-	meshShader.SetUniform("lightColour", glm::vec3(1.0f, 1.0f, 1.0f));
+	meshShader.SetUniform("lightColour", -glm::vec3(1.0f, 1.0f, 1.0f));
 
 	glDrawArrays(GL_TRIANGLES, 0, 108);
 
